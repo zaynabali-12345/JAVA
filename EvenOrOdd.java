3440c9c675743ad4df1107314e9f00f5b086bf95
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-22 15:17:20 +0530

    message

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -2,7 +2,7 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         int number = 15;
         
-        if (number % 2 == 0) {
+        if (number % 2 == 0) {System.out.println("Checking if the number is even or odd...");
             System.out.println(number + " is even.");
         } else {
             System.out.println(number + " is odd.");
