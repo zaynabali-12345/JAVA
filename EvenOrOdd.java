a73f1786806ac43a54483dfdc53758cdbf23b25f
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-22 15:07:45 +0530

    message

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,5 +1,11 @@
-package JAVA;
-
-public class SumOfTwoNumbers {
-
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 15;
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
 }
