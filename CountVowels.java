7cbad9836d46a7fdcbd8b3f293ae54ef30cda4a2
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-22 16:57:06 +0530

    message

--- CountVowels.java
+++ CountVowels.java
@@ -1,5 +1,17 @@
-package JAVA;
-
 public class CountVowels {
-
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int vowelCount = 0;
+        
+        str = str.toLowerCase();
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowelCount++;
+            }
+        }
+        
+        System.out.println("Number of vowels in the string: " + vowelCount);
+    }
 }
