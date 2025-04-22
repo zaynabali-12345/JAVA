5bbcf2f5d1e7067f89e3b45e04bc1e1ef483bcdc
zaynabali-12345 <zaynabali00786@gmail.com>
2025-04-22 15:57:27 +0530

    message

---
+++ ArraySum.java
@@ -0,0 +1,12 @@
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        System.out.println("Sum of the array elements is: " + sum);
+    }
+}
